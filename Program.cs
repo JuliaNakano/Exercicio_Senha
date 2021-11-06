35045ec4c2f54d59fd2ebbc09435ad50a4f157df
JuliaNakano <jumartinsnakano@gmail.com>
2021-11-05 23:33:33 -0300

    Solução do Exercício Senha

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio_Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  senhadigitada;
+            
+            Console.Write("\nOlá, usuário. Por favor, digite sua senha:.. ");
+            senhadigitada = Console.ReadLine();
+
+            if (senhadigitada=="1234abcd")
+            {
+                Console.ForegroundColor =  ConsoleColor.DarkGreen;
+                Console.WriteLine("Acesso permitido\n");
+                Console.ResetColor();
+            }
+            else 
+            {
+               Console.ForegroundColor = ConsoleColor.DarkRed;
+               Console.WriteLine("Acesso negado\n"); 
+               Console.ResetColor();
+            }
+           
+        }
+    }
+}
